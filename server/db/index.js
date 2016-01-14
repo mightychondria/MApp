8834e87aec75c1602f71c31097cb3862070ae59e
JC <jc@jcmbp.local>
2016-01-14 15:51:58 -0800

    perfected database insertion function

--- server/db/index.js
+++ server/db/index.js
@@ -30,59 +30,112 @@ var db = require("seraph")({
 //   });
 // });
 
-// var saveToNodes = function (parent_screen_name, parent_id, screen_name, child_id) {
-//   fs.readFile('../data/' + parent_screen_name + '_' + screen_name, 'utf8', function (err, data) {
-//     if (err) return;
-    
-//     var users = JSON.parse(data).users;
+var saveToNodes = function (parent_screen_name, parent_id, screen_name, child_id) {
+  fs.readFile('../data/' + parent_screen_name + '_' + screen_name, 'utf8', function (err, data) {
+    if (err) return;
 
-//     var nodes = users.map(function (user) {
-//       if (user.status) {
-//         if (user.status.coordinates !== null || user.status.place !== null) {
-//         var node = {};
+    var users = JSON.parse(data).users;
 
-//         node.screen_name = user.screen_name;
-//         node.geo = user.status.coordinates ? user.status.coordinates.coordinates : user.status.place.bounding_box.coordinates[0][0];
-//         node.twitter_id = user.id;
-//         node.followers = user.followers_count;
-//         }
-//       }
-//       return node;
-//     }).filter(function(user) { return user !== undefined; });
+    var nodes = users.map(function (user) {
+      if (user.status) {
+        if (user.status.coordinates !== null || user.status.place !== null) {
+        var node = {};
 
-//   // save to neo4j
-//     nodes.forEach(function (node) {
-//       db.save(node, function (err, savedNode) {
-//         if (err) throw err;
+        node.screen_name = user.screen_name;
+        node.geo = user.status.coordinates ? user.status.coordinates.coordinates : user.status.place.bounding_box.coordinates[0][0];
+        node.twitter_id = user.id;
+        node.followers = user.followers_count;
+        }
+      }
+      return node;
+    }).filter(function(user) { return user !== undefined; });
 
-//         db.relate(child_id, 'follows', savedNode.id, function (err, relationship) {
-//           console.log(relationship);
-//         });
+  // save to neo4j
+    nodes.forEach(function (node) {
 
-//         saveToNodes(screen_name, child_id, savedNode.screen_name, savedNode.id)
-//       });
+      var cypher = ""
+           + "MATCH (n) "
+           + "WHERE n.screen_name='" + node.screen_name + "' "
+           + "RETURN n;";
 
-//     });
-//   });
-//     //double chck
-// };
+      db.query(cypher, function(err, results) {
+        if (err) throw err;
 
-// db.save({'screen_name': 'KimKardashian', 'geo': [-118.659776, 34.127826], 'twitter_id': 25365536, 'followers': 39046765}, function (err, data) {
+        if (results.length > 0) {
+          db.relate(child_id, 'follows', results[0].id, function (err, relationship) {
+            console.log(relationship);
+          });
+        } else {
+          db.save(node, function (err, savedNode) {
+            if (err) throw err;
+
+            db.relate(child_id, 'follows', savedNode.id, function (err, relationship) {
+              console.log(relationship);
+              saveToNodes(screen_name, child_id, savedNode.screen_name, savedNode.id);
+            });
+
+          });
+        }
+      });
+    });
+  });
+    //double chck
+};
+
+db.save({'screen_name': 'KimKardashian', 'geo': [-118.659776, 34.127826], 'twitter_id': 25365536, 'followers': 39046765}, function (err, data) {
+  if (err) throw err;
+  saveToNodes('origin', null, 'KimKardashian', data.id);
+});
+
+
+// var cypher = ""
+//            + "MATCH (n) "
+//            + "WHERE ID(n)=2594 "
+//            + "RETURN n;";
+//            // + "ORDER BY n.name";
+// db.query(cypher, function(err, results) {
 //   if (err) throw err;
-//   saveToNodes('origin', null, 'KimKardashian', data.id);
+//   // console.log("results", results);
+
+//   console.log(results);
+
+//   // results.forEach(function (result) {
+//   //   db.relationships(result.id, 'all', 'follows', function (err, relationships) {
+//   //     console.log(relationships);
+//   //   });
+//   // });
 // });
 
 
-var cypher = ""
-           + "MATCH (n)"
-           + "RETURN n;";
-           // + "ORDER BY n.name";
-db.query(cypher, function(err, results) {
-  if (err) throw err;
-  console.log("results", results);
-  results.forEach(function (result) {
-    db.relationships(result.id, 'all', 'follows', function (err, relationships) {
-      console.log(relationships);
-    });
-  });
-});
+// db.query(cypher, {id: 2770}, function(err, results) {
+//   if (err) throw err;
+//   // console.log("results", results);
+
+//   console.log(results);
+
+//   // results.forEach(function (result) {
+//   //   db.relationships(result.id, 'all', 'follows', function (err, relationships) {
+//   //     console.log(relationships);
+//   //   });
+//   // });
+// });
+
+
+// var cypher = ""
+//      + "MATCH (n) "
+//      + "WHERE n.screen_name=" + "'awdad' "
+//      + "RETURN n;";
+//      // + "ORDER BY n.name";
+// db.query(cypher, function(err, results) {
+//   if (err) throw err;
+//   // console.log("results", results);
+
+//   console.log(Array.isArray(results));
+//   console.log(results.length);
+
+//   // results.forEach(function (result) {
+//   //   db.relationships(result.id, 'all', 'follows', function (err, relationships) {
+//   //     console.log(relationships);
+//   //   });
+//   // });
+// });
