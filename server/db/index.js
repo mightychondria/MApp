c84bf9815c5cb127ec60ec766f4a0e77b0f19270
JC <jc@jcmbp.local>
2016-01-14 14:13:29 -0800

    fixed ids

--- server/db/index.js
+++ server/db/index.js
@@ -15,67 +15,73 @@ var db = require("seraph")({
 //   console.log("Test-Man inserted.");
 // });
 
+// var cypher = ""
+//            + "MATCH (n)"
+//            + "RETURN n;";
+//            // + "ORDER BY n.name";
+
+// db.query(cypher, function(err, results) {
+//   if (err) throw err;
+//   results.forEach(function (node) {
+//     db.delete(node, function(err) {
+//       if (err) throw err;
+//       console.log("deleted", node);
+//     });
+//   });
+// });
+
+var saveToNodes = function (parent_screen_name, parent_id, screen_name, child_id) {
+  fs.readFile('../data/' + parent_screen_name + '_' + screen_name, 'utf8', function (err, data) {
+    var users = JSON.parse(data).users;
+
+    var nodes = users.map(function (user) {
+      if (user.status) {
+        if (user.status.coordinates !== null || user.status.place !== null) {
+        var node = {};
+
+        node.screen_name = user.screen_name;
+        node.geo = user.status.coordinates ? user.status.coordinates.coordinates : user.status.place.bounding_box.coordinates[0][0];
+        node.twitter_id = user.id;
+        node.followers = user.followers_count;
+        }
+      }
+      return node;
+    }).filter(function(user) { return user !== undefined; });
+
+  // save to neo4j
+    nodes.forEach(function (node) {
+      db.save(node, function (err, savedNode) {
+        if (err) throw err;
+        if (parent_id !== null) {
+          db.relate(parent_id, 'follows', savedNode.id, function (err, relationship) {
+            console.log(relationship);
+          });
+        }
+
+        saveToNodes(screen_name, child_id, savedNode.screen_name, savedNode.id)
+      });
+
+    });
+  });
+    //double chck
+};
+
+db.save({'screen_name': 'KimKardashian', 'geo': [-118.659776, 34.127826], 'twitter_id': 25365536, 'followers': 39046765}, function (err, data) {
+  if (err) throw err;
+  saveToNodes('origin', null, 'KimKardashian', data.id);
+});
+
+
 var cypher = ""
            + "MATCH (n)"
            + "RETURN n;";
            // + "ORDER BY n.name";
-
 db.query(cypher, function(err, results) {
   if (err) throw err;
-  results.forEach(function (node) {
-    db.delete(node, function(err) {
-      if (err) throw err;
-      console.log("deleted", node);
+  console.log("results", results);
+  results.forEach(function (result) {
+    db.relationships(result.id, 'all', 'follows', function (err, relationships) {
+      console.log(relationships);
     });
   });
 });
-
-// var saveToNodes = function (parent_screen_name, parent_id, screen_name) {
-//   fs.readFile('../data/origin_KimKardashian', 'utf8', function (err, data) {
-//     var users = JSON.parse(data).users;
-
-//     var nodes = users.map(function (user) {
-//       if (user.status) {
-//         if (user.status.coordinates !== null || user.status.place !== null) {
-//         var node = {};
-
-//         node.screen_name = user.screen_name;
-//         node.geo = user.status.coordinates ? user.status.coordinates.coordinates : user.status.place.bounding_box.coordinates[0][0];
-//         node.twitter_id = user.id;
-//         node.followers = user.followers_count;
-//         }
-//       }
-//       return node;
-//     }).filter(function(user) { return user !== undefined; });
-
-//   // save to neo4j
-//     nodes.forEach(function (node) {
-//       db.save(node, function (err, savedNode) {
-//         if (err) throw err;
-//         db.relate(parent_id, 'follows', savedNode.id, function (err, relationship) {
-//           console.log(relationship);
-//           });
-//         });
-//       });
-//     });
-//     //double chck
-//     var cypher = ""
-//                + "MATCH (n)"
-//                + "RETURN n;";
-//                // + "ORDER BY n.name";
-//     db.query(cypher, function(err, result) {
-//       if (err) throw err;
-//       console.log(result);
-//   });
-
-
-//   // create id1
-//   // for each of id1's children
-//     // create/save idChild
-//     // relate id1, idChild
-// };
-
-// db.save({'screen_name': 'KimKardashian', 'geo': [-118.659776, 34.127826], 'twitter_id': 25365536, 'followers': 39046765}, function (err, data) {
-//   if (err) throw err;
-//   saveToNodes('origin', data.id, 'KimKardashian');
-// })
