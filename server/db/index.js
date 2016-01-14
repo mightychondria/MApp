799f2f42a8f5a5ef44fbea2e69784ffb986d4136
JC <jc@jcmbp.local>
2016-01-14 12:29:58 -0800

    progress on savetonodes

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,58 @@
-var mysql = require('mysql');
+var fs = require('fs');
 
-module.exports = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'wj',
-  database : 'chat'
+// var db = require("seraph")({
+//   user: 'neo4j',
+//   pass: 'test'
+// });
+
+// // db.changePassword('test', function(err) {
+// //   //password is now changed, and `db`'s options have been updated with the new password
+// // });
+
+
+// db.save({ name: "Test-Man2", age: 40, twitter_id: 12392158 }, function (err, node) {
+//   if (err) throw err;
+//   console.log("Test-Man inserted.");
+
+//   var cypher = ""
+//              + "MATCH (n)"
+//              + "RETURN n;";
+//              // + "ORDER BY n.name";
+
+//   db.query(cypher, function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// });
+
+//   db.delete(node, function(err) {
+//     if (err) throw err;
+//     console.log("Test-Man away!");
+//   });
+// });
+
+var saveToNodes = function (parent_screen_name, parent_id, screen_name) {
+  fs.readFile('../data/origin_KimKardashian', 'utf8', function (err, data) {
+    var users = JSON.parse(data).users;
+
+    var nodes = users.map(function (user) {
+      if (user.status) {
+        if (user.status.coordinates !== null || user.status.place !== null) {
+        var node = {};
+
+        node.screen_name = user.screen_name;
+        node.geo = user.status.coordinates ? user.status.coordinates.coordinates : user.status.place.bounding_box.coordinates[0][0];
+        node.twitter_id = user.id;
+        node.followers = user.followers_count;
+        }
+      }
+      return node;
+    }).filter(function(user) { return user !== undefined; });
+  };
+
+  // save to neo4j
+  // create id1
+  // for each of id1's children
+    // create/save idChild
+    // relate id1, idChild
 });
